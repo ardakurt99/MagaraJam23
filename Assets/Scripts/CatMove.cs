14b08afb43b2cce13040ae1f06176d6b2a2ffbf9
Rıdvan Çömez <85356125+ridvancomez@users.noreply.github.com>
2023-11-17 01:59:09 +0300

    Karakter Köntrolü bitti

--- Assets/Scripts/CatMove.cs
+++ Assets/Scripts/CatMove.cs
@@ -12,20 +12,56 @@ public class CatMove : MonoBehaviour
     [SerializeField] private float speed;
     [SerializeField] private float rotationSpeed;
 
-    [SerializeField]
+    [SerializeField] private float targetMaxDistance; 
+    [SerializeField] private List<Target> targets;
+    [SerializeField] private Target target;
 
+    [SerializeField] private bool lockedOnTarget;
     // Start is called before the first frame update
     void Start()
     {
         speed = 10;
+        StartCoroutine(TargetControl());
+
     }
 
     // Update is called once per frame
     void Update()
     {
        transform.Translate(new Vector3(Input.GetAxis("Horizontal"), Input.GetAxis("Jump"), Input.GetAxis("Vertical")) * speed *Time.deltaTime);
-       
-        
 
+       if(lockedOnTarget)
+       {
+        transform.position = Vector3.Lerp((target.transform.position) - Vector3.one * 3, transform.position, .8f);
+       }
+       
+       if(Input.GetKeyDown(KeyCode.Backspace))
+       {
+        lockedOnTarget = false;
+        target.IsActive =false;
+       }
+    }
+
+    IEnumerator TargetControl()
+    {
+        while(true)
+        {
+            if(!lockedOnTarget)
+            {
+                for (int i = 0; i < targets.Count; i++)
+                {
+                    if(targets[i].IsActive && Vector3.Distance(targets[i].gameObject.transform.position, transform.position) < targetMaxDistance)
+                    {
+                        lockedOnTarget = true;
+                        target = targets[i];
+                        break;
+                    }
+                }
+
+               
+            }
+
+             yield return new WaitForSeconds(.5f);
+        }
     }
 }
