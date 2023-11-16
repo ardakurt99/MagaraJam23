d46c9f025265576f7407476c1c3339e2f8e6fb47
Rıdvan Çömez <85356125+ridvancomez@users.noreply.github.com>
2023-11-16 22:52:31 +0300

    Character Control + PlayetControl

--- Assets/Scripts/CharacterController.cs
+++ Assets/Scripts/CharacterController.cs
@@ -2,20 +2,19 @@ using UnityEngine;
 
 public class CharacterControl : MonoBehaviour
 {
-    private float health;
-    protected float Speed { get; set; }
+    [SerializeField] private float health;
+    [SerializeField] private float speed;
+    [SerializeField] protected float Speed { get {return speed;} set{ speed = value;} }
     protected float Health
     {
-        get { return health; } //veriyi okurken çalışan metot
+        get {  return health; } //veriyi okurken çalışan metot
         set //veriyi çekreken çalışan metot
         {
+            
             if (value < 0)
                 health = 0;
             else
                 health = value;
         }
     }
-
-
-
 }
