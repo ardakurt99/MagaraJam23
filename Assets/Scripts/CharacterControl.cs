185f1eb2639e7256dcd13ca7c8387347a11495b5
Rıdvan Çömez <85356125+ridvancomez@users.noreply.github.com>
2023-11-17 11:46:35 +0300

    Karakter Gözlük ve Ateş Etme Mekaniği

---
+++ Assets/Scripts/CharacterControl.cs
@@ -0,0 +1,38 @@
+using UnityEngine;
+
+public class CharacterControl : MonoBehaviour
+{
+    [Header("Ateş Verileri")]
+    [SerializeField, Tooltip("Silahın Ateş etme uzaklığı")] private float range;
+
+    void Start()
+    {
+
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Mouse0))
+        {
+            Shoot();
+        }
+
+        Debug.DrawLine(Camera.main.transform.position, Camera.main.transform.position + (Camera.main.transform.forward * range), Color.black);
+    }
+
+    private void Shoot()
+    {
+        RaycastHit hit;
+
+        if (Physics.Raycast(Camera.main.transform.position, Camera.main.transform.forward, out hit, range))
+        {
+            Rigidbody rb = hit.collider.GetComponent<Rigidbody>();
+
+            if(rb != null)
+            {
+                rb.AddForce(Vector3.forward * 5, ForceMode.Impulse);
+            }
+            
+        }
+    }
+}
