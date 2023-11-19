0418c90417c9ff7d944043641846634a860b00fb
Arda Kurt <60116132+ardakurt99@users.noreply.github.com>
2023-11-19 03:19:42 +0300

    Aa

---
+++ Assets/Scripts/LevelLoader.cs
@@ -0,0 +1,29 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class LevelLoader : MonoBehaviour
+{
+    [SerializeField] private Slider loadSlider;
+    [SerializeField] private GameObject loadingScreen;
+    [SerializeField] private Slider slider;
+
+    public void LoadLevel (int sceneIndex)
+    {
+        StartCoroutine(LoadScene(sceneIndex));
+    }
+
+    System.Collections.IEnumerator LoadScene(int sceneIndex)
+    {
+        AsyncOperation operation = SceneManager.LoadSceneAsync(sceneIndex);
+
+        loadingScreen.SetActive(true);
+
+        while(!operation.isDone)
+        {
+            float progress = Mathf.Clamp01(operation.progress / .9f);
+            loadSlider.value = progress;
+            yield return null;
+        }
+    }
+}
